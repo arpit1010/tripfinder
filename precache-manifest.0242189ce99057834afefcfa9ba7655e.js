99037a389db3e0f7560a276a9a5b68761e8c76af
arpit1010 <mailmeonarpit@gmail.com>
2019-01-23 09:46:29 +0400

    Updates

---
+++ precache-manifest.0242189ce99057834afefcfa9ba7655e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1625cf4d954a6ff3b82",
+    "url": "/tripfinder/static/js/runtime~main.c1625cf4.js"
+  },
+  {
+    "revision": "f0afdb515d213fc39e4b",
+    "url": "/tripfinder/static/js/main.f0afdb51.chunk.js"
+  },
+  {
+    "revision": "8075c3a8622c17a1a457",
+    "url": "/tripfinder/static/js/1.8075c3a8.chunk.js"
+  },
+  {
+    "revision": "f0afdb515d213fc39e4b",
+    "url": "/tripfinder/static/css/main.e5fc183d.chunk.css"
+  },
+  {
+    "revision": "fc464839805c1473dd388fab5a89c039",
+    "url": "/tripfinder/index.html"
+  }
+];
